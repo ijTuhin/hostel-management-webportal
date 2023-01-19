aa374ad873ea8ead63011be87b4f41985200a257
ijTuhin <isratishu0209@gmail.com>
2023-01-19 17:53:07 +0600

    toggle between student and guest registration form

--- src/Authentications/NonResidenceUserSignup.jsx
+++ src/Authentications/NonResidenceUserSignup.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUserSlash } from "react-icons/fa";
 
 const NonResidenceUserSignup = () => {
+  const [toggleStudent, setToggleStudent] = useState(false);
+
   return (
     <div className="flex justify-center mt-[10%]">
       <div className="border-0 shadow-lg space-y-3 bg-white pb-16 pt-12 px-16">
@@ -9,9 +11,9 @@ const NonResidenceUserSignup = () => {
           <span>
             <FaUserSlash />
           </span>
-          Non-Residence Registration (Student)
+          Non-Residence Registration {toggleStudent ? "(Guest)" : "(Student)"}
         </div>
-        <form action="" className="flex flex-col">
+        <form action="" className="flex flex-col gap-y-2">
           <div className="space-x-2">
             <input
               className="border-b-2 border-slate-500 px-2 py-1"
@@ -20,12 +22,38 @@ const NonResidenceUserSignup = () => {
               id=""
               placeholder="Enter your email"
             />
+            {toggleStudent ? (
+              <input
+                className="border-b-2 border-slate-500 px-2 py-1"
+                type="text"
+                name=""
+                id=""
+                placeholder="Enter your NID"
+              />
+            ) : (
+              <input
+                className="border-b-2 border-slate-500 px-2 py-1"
+                type="text"
+                name=""
+                id=""
+                placeholder="Enter your matric Id"
+              />
+            )}
+          </div>
+          <div className="space-x-2">
             <input
               className="border-b-2 border-slate-500 px-2 py-1"
               type="text"
               name=""
               id=""
-              placeholder="Enter your matric Id"
+              placeholder="Enter your name"
+            />
+            <input
+              className="border-b-2 border-slate-500 px-2 py-1"
+              type="text"
+              name=""
+              id=""
+              placeholder="Phone number"
             />
           </div>
           <div className="space-x-2">
@@ -44,34 +72,58 @@ const NonResidenceUserSignup = () => {
               placeholder="Password"
             />
           </div>
-          <div className="flex justify-start items-center gap-x-2">
-            <input
-              className="border-b-2 border-slate-500 px-2 py-1"
-              type="text"
-              name=""
-              id=""
-              placeholder="Enter your batch"
-            />
-            <div>
-              <select
-                className="py-1 text-gray-600 border-b-2 border-slate-500"
+          {toggleStudent ? (
+            <div className="space-x-2">
+              <input
+                className="border-b-2 border-slate-500 px-2 py-1"
+                type="text"
                 name=""
                 id=""
-              >
-                <option value="Choose Your Department">
-                  Choose Your Department
-                </option>
-                <option value="Student">Student</option>
-                <option value="Guest">Guest</option>
-              </select>
+                placeholder="Reference residence Id"
+              />
+              <input
+                className="border-b-2 border-slate-500 px-2 py-1"
+                type="text"
+                name=""
+                id=""
+                placeholder="Relation with residence"
+              />
             </div>
-          </div>
+          ) : (
+            <div className="flex justify-start items-center gap-x-2">
+              <input
+                className="border-b-2 border-slate-500 px-2 py-1"
+                type="text"
+                name=""
+                id=""
+                placeholder="Enter your batch"
+              />
+              <div>
+                <select
+                  className="py-1 text-gray-600 border-b-2 border-slate-500"
+                  name=""
+                  id=""
+                >
+                  <option value="Choose Your Department">
+                    Choose Your Department
+                  </option>
+                  <option value="Student">Student</option>
+                  <option value="Guest">Guest</option>
+                </select>
+              </div>
+            </div>
+          )}
           <div className="text-gray-600 pt-1.5 pl-1">
             <span>Choose your picture:</span>
             <input className="text-xs pl-1.5" type="file" name="" id="" />
           </div>
           <div className="flex justify-end mt-2.5 gap-x-1 items-end">
-            <input type="checkbox" name="" id="" />
+            <input
+              onChange={(e) => setToggleStudent(!toggleStudent)}
+              type="checkbox"
+              name=""
+              id=""
+            />
             <p className="text-xs">Guest?</p>
           </div>
           <button className="border-0 w-full bg-slate-600 hover:bg-slate-800 text-gray-200 rounded mt-2.5 px-3 py-1.5">
