1613d1f13ceed739cae49681d885bc0d70a44b99
ijTuhin <isratishu0209@gmail.com>
2023-02-04 19:33:58 +0600

    Navbar logo added

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import { IoMdArrowDropright, IoMdArrowDropdown, IoIosLogOut } from "react-icons/io";
 import ProfileDropdown from "./ProfileDropdown";
+import logo from "../../Images/NavLogo.png";
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
   return (
     <section className="fixed w-full">
       <nav className="flex justify-between p-5 bg-black text-gray-300 font-medium relative">
-        <div>wqrq</div>
+        <div className="flex justify-center items-center">
+            <img className="w-8 h-8 rounded-full" src={logo} alt="" />
+            <p className="font-mono px-2 text-green-400 font-semibold">MessMeal</p>
+        </div>
         <ul className="flex space-x-5 ">
           <li className="hover:text-yellow-600">Home</li>
           <li className="hover:text-yellow-600">Order Summary</li>
