acb926653d1a4f1e7ce225bfff1739b5e8a8199d
ijTuhin <isratishu0209@gmail.com>
2023-02-13 00:10:47 +0600

    link route between login-signup page

--- src/Authentications/LoginForm/LoginPage.jsx
+++ src/Authentications/LoginForm/LoginPage.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 import bgLogo from "../../Images/login-II.jpg";
-import Login from './Login';
+import Login from "./Login";
 
 const LoginPage = () => {
-    return (
+  return (
+    <div className="relative">
+      <div className="w-full p-6 text-green-600 text-sm font-mono flex justify-end absolute">
+        <Link to="/user-signup">
+          <span className="hover:underline px-3 py-1.5">Don't have a account?</span>
+        </Link>
+      </div>
       <div className="flex justify-center items-center">
         <div className="w-[40%] h-screen flex items-center">
           <img src={bgLogo} className="w-[75%]" alt="" />
         </div>
         {/* Form */}
-        <Login/>
+        <Login />
       </div>
-    )
-  }
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/Authentications/SignupForms/SignupPage.jsx
+++ src/Authentications/SignupForms/SignupPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import bgLogo from "../../Images/signup-I.jpg";
 import Signup from "./Signup";
 import UserData from "./UserData";
@@ -6,17 +7,23 @@ const SignupPage = () => {
   const [signupData, setSignupData] = useState();
   const [createAcc, setCreateAcc] = useState(false);
   return (
-    <div className="flex justify-center items-center">
-      <div className="w-[40%] h-screen flex items-center">
-        <img src={bgLogo} className="w-[75%]" alt="" />
+    <div className="relative">
+      <div className="w-full p-6 text-green-600 text-sm font-mono flex justify-end absolute">
+        <Link to="/user-login">
+          <span className="hover:underline px-3 py-1.5">Already a user ?</span>
+        </Link>
       </div>
-      {/* Form */}
-      <Signup/>
-      {/* {createAcc ? (
-        <Signup setSignupData={setSignupData} setCreateAcc={setCreateAcc} />
-      ) : (
-        <UserData setSignupData={setSignupData} setCreateAcc={setCreateAcc} />
-      )} */}
+      <div className="flex justify-center items-center">
+        <div className="w-[40%] h-screen flex items-center">
+          <img src={bgLogo} className="w-[75%]" alt="" />
+        </div>
+        {/* Form */}
+        {createAcc ? (
+          <Signup setSignupData={setSignupData} setCreateAcc={setCreateAcc} />
+        ) : (
+          <UserData setSignupData={setSignupData} setCreateAcc={setCreateAcc} />
+        )}
+      </div>
     </div>
   );
 };
