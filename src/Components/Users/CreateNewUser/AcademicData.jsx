b551d1c01708343dbbd187762f11dde08719af29
ijTuhin <isratishu0209@gmail.com>
2023-03-28 20:34:46 +0600

    react hook form to create component

--- src/Components/Users/CreateNewUser/AcademicData.jsx
+++ src/Components/Users/CreateNewUser/AcademicData.jsx
@@ -1,41 +1,102 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { NavLink } from "react-router-dom";
 
 const AcademicData = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <div className='w-full h-full py-14 px-40 space-y-4 bg-gray-50'>
-        <div className='flex justify-between items-center border-b border-slate-200 py-3.5'>
-            <div>
-                <p className='text-xl font-semibold font-serif text-gray-600'>Student Information</p>
-                <p className='text-sm text-gray-400'>(Individual data)</p>
-            </div>
-            <div className='flex flex-col gap-y-3'>
-                <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-                <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-                <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-            </div>
+    <div className="w-full h-full py-14 px-40 bg-gray-50">
+      <form onSubmit={handleSubmit(onSubmit)} className=" space-y-4">
+        <div className="flex justify-between items-center border-b border-slate-200 py-3.5">
+          <div>
+            <p className="text-xl font-semibold font-serif text-gray-600">
+              Student Information
+            </p>
+            <p className="text-sm text-gray-400">(Individual data)</p>
+          </div>
+          <div className="flex flex-col gap-y-3">
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded"
+              placeholder="Enter name"
+              {...register("name")}
+            />
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded"
+              placeholder="Enter Gsuite"
+              {...register("Gsuite", { required: true })}
+            />
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded"
+              placeholder="Matric ID"
+              {...register("matric")}
+            />
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded"
+              placeholder="Semester"
+              {...register("sem")}
+            />
+          </div>
         </div>
-        <div className='flex justify-between items-center border-b border-slate-200 py-3.5'>
-            <p className='text-xl font-semibold font-serif text-gray-600'>Department</p>
-            <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
+        <div className="flex justify-between items-center border-b border-slate-200 py-3.5">
+          <p className="text-xl font-semibold font-serif text-gray-600">
+            Department
+          </p>
+          <select
+            {...register("dept")}
+            id="countries"
+            className="border-2 border-slate-300 outline-teal-500 bg-white p-2 w-80 rounded"
+          >
+            <option disabled>Choose Department</option>
+            <option value="CSE">CSE</option>
+            <option value="Pharmacy">Pharmacy</option>
+            <option value="EEE">EEE</option>
+            <option value="BBA">BBA</option>
+            <option value="EB">EB</option>
+            <option value="ELL">ELL</option>
+          </select>
+          {/* <input
+            className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded"
+            type="text"
+            name=""
+            id=""
+          /> */}
         </div>
-        <div className='flex justify-between items-center border-b border-slate-200 py-3.5'>
-            <div>
-                <p className='text-xl font-semibold font-serif text-gray-600'>Academic Session</p>
-                <p className='text-sm text-gray-400'>(Current session and the session to complete the program)</p>
-            </div>
-            <div className='flex flex-col gap-y-3'>
-                <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-                <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-            </div>
+        <div className="flex justify-between items-center border-b border-slate-200 py-3.5">
+          <div>
+            <p className="text-xl font-semibold font-serif text-gray-600">
+              Academic Session
+            </p>
+            <p className="text-sm text-gray-400">
+              (Current session and the session to complete the program)
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-3">
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded"
+              placeholder="Current Session"
+              {...register("current")}
+            />
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded"
+              placeholder="Last Session"
+              {...register("last")}
+            />
+          </div>
         </div>
-        <div className='w-full flex justify-end'>
-            <button className='border-2 rounded-md px-6 py-1.5 bg-teal-600 hover:bg-teal-700 text-white'>
-                <NavLink to="/users/create/personal">Next</NavLink>
-            </button>
+        <div className="w-full flex justify-end">
+          <button className="border-2 rounded-md px-6 py-1.5 bg-teal-600 hover:bg-teal-700 text-white">
+            <NavLink to="/users/create/personal">Next</NavLink>
+          </button>
         </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AcademicData
+export default AcademicData;
--- src/Components/Users/CreateNewUser/PersonalData.jsx
+++ src/Components/Users/CreateNewUser/PersonalData.jsx
@@ -1,38 +1,71 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { NavLink } from "react-router-dom";
 
 const PersonalData = () => {
-    return (
-        <div className='w-full h-full py-14 px-40 space-y-4 bg-gray-50'>
-            <div className='flex justify-between items-center border-b border-slate-200 py-3.5'>
-                <div>
-                    <p className='text-xl font-semibold font-serif text-gray-600'>Personal Information</p>
-                    <p className='text-sm text-gray-400'></p>
-                </div>
-                <div className='flex flex-col gap-y-3'>
-                    <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-                    <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-                </div>
-            </div>
-            <div className='flex justify-between items-center border-b border-slate-200 py-3.5'>
-                <div>
-                    <p className='text-xl font-semibold font-serif text-gray-600'>Permanent Address</p>
-                    <p className='text-sm text-gray-400'>(For emergency, community, forum, etc.)</p>
-                </div>
-                <div className='flex flex-col gap-y-3'>
-                    <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-                    <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-                    <input className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-                </div>
-            </div>
-            <input hidden className='border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-80 rounded' type="text" name="" id="" />
-            <div className='w-full flex justify-end'>
-                <button className='border-2 rounded-md px-6 py-1.5 bg-teal-600 hover:bg-teal-700 text-white'>
-                    <NavLink to="/users/create/overview">Next</NavLink>
-                </button>
-            </div>
-        </div>
-      )
-}
+  const d = new Date();
+  const date = d.getDate();
+  const month = d.getMonth() + 1;
+  const year = d.getFullYear();
+  const enrolled = date + "/" + month + "/" + year;
 
-export default PersonalData
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className="w-full h-full py-32 px-40  bg-gray-50">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="space-y-1 flex justify-end items-end"
+      >
+        <div className="space-y-5 border-t border-teal-600 py-3.5">
+          <div>
+            <p className="text-xl font-semibold font-serif text-teal-600">
+              Personal Information
+            </p>
+            <p className="text-sm text-gray-400">
+              (For emergency, community, forum, etc.)
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-3">
+            <input hidden value={enrolled} {...register("enrolled")} />
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-96 rounded"
+              placeholder="Phone number"
+              {...register("phone")}
+            />
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-96 rounded"
+              placeholder="District"
+              {...register("district")}
+            />
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-96 rounded"
+              placeholder="Thana"
+              {...register("thana")}
+            />
+            <input
+              className="border-2 border-slate-300 outline-teal-500 bg-white px-2 py-1.5 w-96 rounded"
+              placeholder="Address"
+              {...register("address")}
+            />
+          </div>
+        </div>
+        <div className="w-full flex justify-end pb-3 gap-x-2">
+          <button className="border-2 rounded-md px-4 py-1.5 bg-gray-400 hover:bg-gray-500 text-gray-50">
+            <NavLink to="/users/create/academic">Cancel</NavLink>
+          </button>
+          <button className="border-2 rounded-md px-6 py-1.5 bg-teal-600 hover:bg-teal-700 text-white">
+            <NavLink to="/users/create/overview">Next</NavLink>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PersonalData;
