6a39c02ca4eef2727bfbef5df02e76954fa0f68e
ijTuhin <isratishu0209@gmail.com>
2023-05-05 20:53:02 +0600

    POST data to DB successful

--- src/Components/Users/CreateNewUser/Overview.jsx
+++ src/Components/Users/CreateNewUser/Overview.jsx
@@ -8,11 +8,25 @@ const Overview = () => {
   const month = d.getMonth() + 1;
   const year = d.getFullYear();
   const enrolled = d.getDate() + "/" + month + "/" + d.getFullYear();
-  const updateData = () => {
+  const handlePostData = () => {
     setValue({
       ...value,
       enrollDate: enrolled,
     });
+
+    /* ************************************ */
+    fetch("http://localhost:5000/user", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(value),
+    })
+      .then((response) => response.json())
+      .then((value) => {
+        console.log("DB Success:", value);
+      });
+    /* ************************************ */
   };
   console.log("Overview ",value)
   return (
@@ -63,8 +77,8 @@ const Overview = () => {
             </p>
             <div className="flex gap-x-6">
               <p>
-                Thana:{value.thana}
-                <span className="text-sm text-gray-600 px-2">Chawkbazar</span>
+                Thana:
+                <span className="text-sm text-gray-600 px-2">{value.thana}</span>
               </p>
               <p>
                 District:
@@ -84,7 +98,7 @@ const Overview = () => {
         <button className="border-2 rounded-md px-6 py-1.5 bg-gray-300 hover:bg-gray-500 text-gray-50">
           <NavLink to="/users/create/academic">Cancel</NavLink>
         </button>
-        <button onClick={updateData} type="button" className="border-2 rounded-md px-8 py-1.5 bg-teal-600 hover:bg-teal-700 text-white">
+        <button onClick={handlePostData} type="button" className="border-2 rounded-md px-8 py-1.5 bg-teal-600 hover:bg-teal-700 text-white">
           {/* <NavLink to="/users/create/account">Save</NavLink> */}
         </button>
       </div>
