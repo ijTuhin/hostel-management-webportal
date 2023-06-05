16f1b5ca033d9809f53d07e7f22e1a9b41351e0c
ijTuhin <isratishu0209@gmail.com>
2023-06-05 23:28:27 +0600

    Add grocery item done

--- src/Components/Meal/Groceries/GroceriesPage.jsx
+++ src/Components/Meal/Groceries/GroceriesPage.jsx
@@ -6,7 +6,6 @@ import { useLoaderData } from "react-router-dom";
 const GroceriesPage = () => {
   const date = new Date().toLocaleDateString();
   const data = useLoaderData();
-  console.log(data);
   return (
     <div className="px-32 py-6 w-full space-y-8">
       <AddItemForm />
@@ -14,9 +13,13 @@ const GroceriesPage = () => {
         <div className="py-3 w-full bg-emerald-700/25 text-center text-lg">
           List of Groceries - {date}
         </div>
-        {data[0].list.map((item) => (
-          <TableData key={item._id} item={item} />
-        ))}
+        <div className="w-full flex justify-center">
+          <div className="w-fit">
+            {data[0].list.map((item) => (
+              <TableData key={item._id} item={item} />
+            ))}
+          </div>
+        </div>
         <div className="w-full flex justify-end border-t border-blue-900/50 px-1 py-1">
           <p>
             Total Cost = <span>{data[0].total} /-</span>
